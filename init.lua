b1aa1a58c80136c5fba1a73c78f3a25e967f5844
François Freitag <mail@franek.fr>
2024-06-11 09:05:16 +0200

    nvim: Lazily configure plugins

--- init.lua
+++ init.lua
@@ -1,18 +1,41 @@
 require("config")
 require("install_lazy")
 require("lazy").setup({
-	"neovim/nvim-lspconfig",
-	"nvim-treesitter/nvim-treesitter",
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lsp")
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("treesitter")
+		end,
+	},
 	"nvim-treesitter/nvim-treesitter-context",
 	"nvim-treesitter/nvim-treesitter-textobjects",
 	{
 		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("fzf")
+		end,
 	},
-	"stevearc/conform.nvim",
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform_config")
+		end,
+	},
 	"vim-scripts/matchit.zip",
 	"wellle/targets.vim",
-	"lewis6991/gitsigns.nvim",
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns_config")
+		end,
+	},
 	"tpope/vim-fugitive",
 	"tpope/vim-rhubarb",
 	"tpope/vim-commentary",
@@ -25,6 +48,49 @@ require("lazy").setup({
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				component_separators = "",
+				section_separators = "",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = {
+					"branch",
+					{
+						"diff",
+						colored = false,
+					},
+					{
+						"diagnostics",
+						colored = false,
+					},
+				},
+				lualine_c = {
+					{
+						"filename",
+						path = 1, -- Relative path
+						shorting_target = 20,
+					},
+				},
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			tabline = {
+				lualine_a = {
+					{
+						"tabs",
+						mode = 1, -- Shows tab_name
+					},
+				},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		},
 	},
 	{
 		"craftzdog/solarized-osaka.nvim",
@@ -33,14 +99,7 @@ require("lazy").setup({
 		opts = {},
 	},
 })
-
 vim.cmd("colorscheme solarized-osaka")
-require("lsp")
-require("treesitter")
-require("conform_config")
-require("fzf")
-require("gitsigns_config")
-require("lualine_config")
 
 vim.cmd([[
 " Filetype specific settings

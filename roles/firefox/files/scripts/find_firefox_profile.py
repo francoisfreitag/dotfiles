4e868fd1ceda8966ee37ae2985f73490323164e8
François Freitag <mail@franek.fr>
2025-02-12 22:18:23 +0100

    firefox: Setup user prefs and styles

---
+++ roles/firefox/files/scripts/find_firefox_profile.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import configparser
+import pathlib
+import sys
+
+
+def main(config):
+    for section in config.sections():
+        try:
+            default = config[section]["Default"]
+        except KeyError:
+            pass
+        else:
+            if default.endswith(".dev-edition-default"):
+                return default
+    sys.exit("Could not find default firefox-dev-edition profile.")
+
+
+if __name__ == "__main__":
+    profiles_path = pathlib.Path().home() / ".mozilla" / "firefox" / "profiles.ini"
+    config = configparser.ConfigParser()
+    config.read(profiles_path)
+    print(main(config))

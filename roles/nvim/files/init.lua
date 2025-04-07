3cabf535bfead402d4257a13a082c4d74af3d9d4
François Freitag <mail@franek.fr>
2025-04-07 13:57:21 +0200

    nvim: Set a max_lines to tree-sitter-context
    Otherwise, Pulumi or JSON context goes up to the cursor, preventing from
    reading the few previous lines.

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -8,7 +8,12 @@ require("lazy").setup({
 			require("treesitter")
 		end,
 	},
-	"nvim-treesitter/nvim-treesitter-context",
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {
+			max_lines = 8,
+		},
+	},
 	"nvim-treesitter/nvim-treesitter-textobjects",
 	{
 		"ibhagwan/fzf-lua",

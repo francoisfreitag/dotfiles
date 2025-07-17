d6b211065dfb7fcf8b8579a4df4808eff82217c6
François Freitag <mail@franek.fr>
2025-07-17 11:42:58 +0200

    nvim: Limit quickfile mappings to the qf buffer
    Otherwise, would override ]q for other buffers.

--- roles/nvim/files/ftplugin/qf.lua
+++ roles/nvim/files/ftplugin/qf.lua
@@ -1,4 +1,4 @@
 vim.cmd([[packadd cfilter]])
-vim.keymap.set("n", "gq", ":cclose<CR>")
-vim.keymap.set("n", "]f", ":cnfile<CR>")
-vim.keymap.set("n", "[f", ":cpfile<CR>")
+vim.keymap.set("n", "gq", ":cclose<CR>", { buffer = true })
+vim.keymap.set("n", "]f", ":cnfile<CR>", { buffer = true })
+vim.keymap.set("n", "[f", ":cpfile<CR>", { buffer = true })
